cfcbfcb2de353e914efeaaca1898322b4da05e0c
Md Reazul Islam <reazulislam137@gmail.com>
2019-02-26 21:42:06 +0600

    Add files via upload

---
+++ Python File Input Output/output 6.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb 26 21:25:38 2019
+
+@author: Md Reazul Islam
+"""
+
+# Print
+print()
+print("hello"*3)
+print("All the power \n is with in you")
+a,b=20,30
+print(a+b)
+
+# Print and string formate
+
+name  = "Md. Reazul Islam"
+marks = 1.97
+print ("Name is",name, "And CGPA is", marks)
+print ("Name is %s, Marks are %.2f"%(name,marks))
+print ("Name is {}, Marks are {}".format(name,marks))
