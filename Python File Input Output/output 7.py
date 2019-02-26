4f2b33035b6dd2d70b010e3942028fbcdc7f8676
Md Reazul Islam <reazulislam137@gmail.com>
2019-02-26 21:48:57 +0600

    Add files via upload

---
+++ Python File Input Output/output 7.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb 26 21:42:14 2019
+
+@author: Md Reazul Islam
+"""
+s=input()
+print(s)
+
+s=input("Enter you name : ")
+print(s)
+
+
+i=input("Enter a integer number")
