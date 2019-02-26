2eaa1cfed286a2c0be52875c1d45f09479981c6e
Md Reazul Islam <reazulislam137@gmail.com>
2019-02-26 21:55:39 +0600

    Add files via upload

---
+++ Python File Input Output/output 8.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb 26 21:49:21 2019
+
+@author: Md Reazul Islam
+"""
+
+lst = [x for x in input ("Enter three numbers seperated by space : ").split(',')]
+print(lst)
