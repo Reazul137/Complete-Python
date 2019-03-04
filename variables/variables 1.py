23dd2b73b915fe9407895b21c3b587a21304f886
Md Reazul Islam <reazulislam137@gmail.com>
2019-03-04 16:20:35 +0600

    Add files via upload

---
+++ variables/variables 1.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar  4 16:18:05 2019
+
+@author: Md Reazul Islam
+"""
+
+Item_name = "Computer" #A string
+Item_qty = 10 #An integer
+Item_value = 1000.23 #A floating point
+
+print(Item_name)
+print(Item_qty)
+print(Item_value)
