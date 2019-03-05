c0f538fd4e337d9278d1b421910d7272eff7d657
Md Reazul Islam <reazulislam137@gmail.com>
2019-03-05 22:11:33 +0600

    Add files via upload

---
+++ Beginner Python exercises/char input.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar  5 22:06:52 2019
+
+@author: Md Reazul Islam
+"""
+
+name = input("What is your name : ")
+age  = int  (input("How old are you   : "))
+year = str  ((2014 - age) + 100)
+
+print(name+ " will be 100 years old in the year " + year)
