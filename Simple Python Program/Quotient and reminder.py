02b68827152426007d2b0b7f7105a8ad853537d3
Md Reazul Islam <reazulislam137@gmail.com>
2019-04-02 13:02:11 +0600

    Add files via upload

---
+++ Simple Python Program/Quotient and reminder.py
@@ -0,0 +1,11 @@
+a = input("Enter the first number : ")
+a = int(a)
+
+b = input("Enter the second number : ")
+b = int(b)
+
+quotient = a//b
+remainder = a%b
+
+print("Quotient is : ", quotient)
+print("Remainder is : ", remainder)
