7fd49b136442bdf7a96615323c2f7504511f85a3
Md Reazul Islam <reazulislam137@gmail.com>
2019-04-04 00:52:53 +0600

    Add files via upload

---
+++ Loop Basic/00.py
@@ -0,0 +1,5 @@
+for i in range(4):
+    print("I want to be a python programmer!! In Sah Allah <3 ")
+
+    for i in range(5):
+        print(i)
---
+++ Loop Basic/03.py
@@ -0,0 +1,5 @@
+result = 0
+for i in range(50):
+    result = result+1
+
+    print("1 + ",result,"=",result+1)
