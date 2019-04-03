f4d7257037b5dae35d490c9c6d252a3e9c15774c
Md Reazul Islam <reazulislam137@gmail.com>
2019-04-03 12:33:35 +0600

    Add files via upload

--- Turtle Baisc/01.py
+++ Turtle Baisc/01.py
@@ -1,10 +1,15 @@
 import turtle
 
 turtle.shape("turtle")
-turtle.speed(2)
+turtle.speed(1)
 
-turtle.forward(200)
-turtle.left(120)
-turtle.forward(200)
-turtle.left(120)
-turtle.forward(200)
+turtle.forward(100)
+turtle.left(90)
+turtle.forward(100)
+turtle.left(90)
+turtle.forward(100)
+turtle.left(90)
+turtle.forward(100)
+turtle.left(90)
+
+turtle.exitonclick()
